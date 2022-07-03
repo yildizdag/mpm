568a6097060d8d85964bb22b02ff52c26f979910
yildizdag <yildizdag@berkeley.edu>
2022-07-03 23:12:01 +0300

    parallel IGAFEM

---
+++ Benchmark/main.py
@@ -0,0 +1,19 @@
+import petsc4py
+import slepc4py
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpi4py import MPI
+from patch import *
+
+petsc4py.init(sys.argv)
+
+from petsc4py import PETSc
+from slepc4py import SLEPc
+
+comm = MPI.COMM_WORLD
+
+pf = patchFamily2D('igabem_DR05_4th_768_',2)
+print("##################")
+print("PATCH FAMILY INFO:")
+print("Number of Patches = ",pf.np)
