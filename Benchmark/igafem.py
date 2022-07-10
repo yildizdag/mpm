a8b434d97377ff83d5bad99af3d5a8ed15d39faa
yildizdag <yildizdag@berkeley.edu>
2022-07-10 08:37:47 +0300

    igafem connectivity

--- Benchmark/igafem.py
+++ Benchmark/igafem.py
@@ -40,8 +40,9 @@ class igafem(bc,readPatch2D):
                     LM_p[j,k,:] = self.ID[i][self.IEN[i][j,k],:]
                     self.LM.append(LM_p)
         # FEM PROCEDURE
-        def global_stiffness(self):
+        def global_stiffness(self,ngp):
 
+            x_gp, w_gp = self.quad2D(ngp)
             K = PETSc.Mat().create(comm=comm)
             K.setSizes((self.dof[0],self.dof[0]))
             K.setFromOptions()
@@ -49,5 +50,32 @@ class igafem(bc,readPatch2D):
             for i in range(0,self.nel[0]):
                 el_CP = self.getCP(0,i)
                 el_LM = self.LM[0][i,:,:]
-                #k_local = self.local_stiffness()
+                k_local = self.local_stiffness()
             return K
+
+        def quad1D(self,ngp):
+            if ngp == 1:
+                xgp = [0.0]
+                wgp = [2.0]
+            elif ngp == 2:
+                xgp = [-1./sqrt(3.), 1./sqrt(3.)];
+                wgp = [1., 1.];
+            elif ngp ==3:
+                xgp = [-sqrt(3./5), 0., sqrt(3./5)];
+                wgp = [5./9, 8./9, 5./9];
+
+            return xgp, wgp
+
+        def quad2D(self,ngp):
+            x_ksi, w_ksi = self.quad1D(ngp)
+            x_eta, w_eta = self.quad1D(ngp)
+            x_gp = np.zeros((len(x_ksi)*len(x_eta)),2)
+            w_gp = np.zeros(len(x_ksi)*len(x_eta))
+            for i in range(0,len(x_eta)):
+                k = i*len(x_ksi)
+                for j in range(0,len(x_ksi)):
+                    x_gp[j+k,:] = [x_ksi[j], x_eta[i]]
+                    w_gp[j+k] = w_ksi[j]*w_eta[i]
+            return x_gp, w_gp
+
+        def local_stiffness(self,iu,iv,el_CP,x_gp,w_gp):
